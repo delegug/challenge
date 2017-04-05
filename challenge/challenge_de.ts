2569c2989466c8a4b7fb443cbdffe9267b64e88c
Matthias Gugenhan <matthias@gugenhan.net>
2017-04-05 20:04:23 +0200

    Add documentation and bin folder

---
+++ challenge/challenge_de.ts
@@ -0,0 +1,163 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>ProcessorSelector</name>
+    <message>
+        <location filename="ProcessorSelector.qml" line="45"/>
+        <source>Processor</source>
+        <translation>Prozessor</translation>
+    </message>
+</context>
+<context>
+    <name>linuxCpuInfo</name>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="42"/>
+        <source>Processor</source>
+        <translation>Prozessor</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="43"/>
+        <source>Vendor Id</source>
+        <translation>Hersteller Id</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="44"/>
+        <source>CPU family</source>
+        <translation>CPU Familie</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="45"/>
+        <source>Model</source>
+        <translation>Modell</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="46"/>
+        <source>Model name</source>
+        <translation>Modellbezeichnung</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="47"/>
+        <source>Stepping</source>
+        <translation>Stepping</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="48"/>
+        <source>Microcode</source>
+        <translation>Mikrocode</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="49"/>
+        <source>CPU MHz</source>
+        <translation>CPU MHz</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="50"/>
+        <source>Cache size</source>
+        <translation>Cache Größe</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="51"/>
+        <source>Physical id</source>
+        <translation>Physikalische Id</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="52"/>
+        <source>Siblings</source>
+        <translation>Geschwister</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="53"/>
+        <source>Core Id</source>
+        <translation>Prozessorkern Id</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="54"/>
+        <source>Cpu cores</source>
+        <translation>CPU Kerne</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="55"/>
+        <source>Apicid</source>
+        <translation>Apicid</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="56"/>
+        <source>Initial apicid</source>
+        <translation>Start Apicid</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="57"/>
+        <source>Fpu</source>
+        <translation>Fpu</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="58"/>
+        <source>Fpu Exception</source>
+        <translation>Fpu Ausnahme</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="59"/>
+        <source>Cpuid level</source>
+        <translation>Cpuid Level</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="60"/>
+        <source>Wp</source>
+        <translation>Wp</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="61"/>
+        <source>Flags</source>
+        <translation>Flags</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="62"/>
+        <source>Bugs</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="63"/>
+        <source>Bogomips</source>
+        <translation>Bogomips</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="64"/>
+        <source>Clflush size</source>
+        <translation>Clflush Größe</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="65"/>
+        <source>Cache Alignment</source>
+        <translation>Cache Anordnung</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="66"/>
+        <source>Address sizes</source>
+        <translation>Adresse Größen</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="67"/>
+        <source>Power management</source>
+        <translation>Stromüberwachung</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="97"/>
+        <source>Error initialize the application. Cannot open file &apos;%1&apos;!</source>
+        <translation>Fehler beim Starten der Anwendung. Datei &apos;%1&apos; kann nicht geöffnet werden!</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="110"/>
+        <source>Error initialize the application. File &apos;%1&apos; is empty!</source>
+        <translation>Fehler beim Starten der Anwendung. Datei &apos;%1&apos; ist leer!</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.qml" line="9"/>
+        <source>Challenge</source>
+        <translation>Herausforderung</translation>
+    </message>
+</context>
+</TS>
---
+++ challenge/challenge_en.ts
@@ -0,0 +1,163 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>ProcessorSelector</name>
+    <message>
+        <location filename="ProcessorSelector.qml" line="45"/>
+        <source>Processor</source>
+        <translation>Processor</translation>
+    </message>
+</context>
+<context>
+    <name>linuxCpuInfo</name>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="42"/>
+        <source>Processor</source>
+        <translation>Processor</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="43"/>
+        <source>Vendor Id</source>
+        <translation>Vendor Id</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="44"/>
+        <source>CPU family</source>
+        <translation>CPU Family</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="45"/>
+        <source>Model</source>
+        <translation>Model</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="46"/>
+        <source>Model name</source>
+        <translation>Model name</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="47"/>
+        <source>Stepping</source>
+        <translation>Stepping</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="48"/>
+        <source>Microcode</source>
+        <translation>Microcode</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="49"/>
+        <source>CPU MHz</source>
+        <translation>CPU MHz</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="50"/>
+        <source>Cache size</source>
+        <translation>Cache size</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="51"/>
+        <source>Physical id</source>
+        <translation>Physical id</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="52"/>
+        <source>Siblings</source>
+        <translation>Siblings</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="53"/>
+        <source>Core Id</source>
+        <translation>Core Id</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="54"/>
+        <source>Cpu cores</source>
+        <translation>Cpu cores</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="55"/>
+        <source>Apicid</source>
+        <translation>Apicid</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="56"/>
+        <source>Initial apicid</source>
+        <translation>Initial apicid</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="57"/>
+        <source>Fpu</source>
+        <translation>Fpu</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="58"/>
+        <source>Fpu Exception</source>
+        <translation>Fpu Exception</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="59"/>
+        <source>Cpuid level</source>
+        <translation>Cpuid level</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="60"/>
+        <source>Wp</source>
+        <translation>Wp</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="61"/>
+        <source>Flags</source>
+        <translation>Flags</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="62"/>
+        <source>Bugs</source>
+        <translation>Bugs</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="63"/>
+        <source>Bogomips</source>
+        <translation>Bogomips</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="64"/>
+        <source>Clflush size</source>
+        <translation>Clflush size</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="65"/>
+        <source>Cache Alignment</source>
+        <translation>Cache Alignment</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="66"/>
+        <source>Address sizes</source>
+        <translation>Address sizes</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="67"/>
+        <source>Power management</source>
+        <translation>Power management</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="97"/>
+        <source>Error initialize the application. Cannot open file &apos;%1&apos;!</source>
+        <translation>Error initialize the application. Cannot open file &apos;%1&apos;!</translation>
+    </message>
+    <message>
+        <location filename="linuxcpuinfo.cpp" line="110"/>
+        <source>Error initialize the application. File &apos;%1&apos; is empty!</source>
+        <translation>Error initialize the application. File &apos;%1&apos; is empty!</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.qml" line="9"/>
+        <source>Challenge</source>
+        <translation>Challenge</translation>
+    </message>
+</context>
+</TS>
